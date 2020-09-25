2cc548eae4088778ee0bac44f5a476771e631fcd
Eduardo Cerqueira <bold022@einstein.br>
2020-09-24 21:56:18 -0300

    refactor user controller and repo

--- src/repository/UserRepository.js
+++ src/repository/UserRepository.js
@@ -1,5 +1,4 @@
 import Users from '../models/Users';
-import { handleDefaultError } from '../utils/handleErrors';
 
 class UserRepository {
   async store(user, callback = () => null) {
@@ -21,35 +20,12 @@ class UserRepository {
     return user;
   }
 
-  async checkEmailExists(req, res) {
-    const { email } = req.query;
-
-    try {
-      const user = await _getByEmail(email);
-
-      res.status(200).json({
-        data: { exist: Boolean(user) },
-        msg: user ? 'E-mail existe!' : 'E-mail não existe',
-      });
-    } catch (error) {
-      return handleDefaultError(error, res);
-    }
-  }
-
   async getById(id, callback) {
     const user = await Users.findByPk(id);
     callback(user);
 
     return user;
   }
-}
-
-async function _getByEmail(email) {
-  const user = await Users.findOne({
-    where: { email },
-  });
-
-  return user;
 }
 
 export default new UserRepository();
