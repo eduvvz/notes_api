cd529c75db0ef393013cde26bcc8c8e9b0ffedf2
Eduardo Cerqueira <bold022@einstein.br>
2020-09-24 22:24:08 -0300

    refactor note controller and repo

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -1,10 +1,28 @@
 import NoteValidations from './validators/NoteValidations';
 import NoteRepository from '../repository/NoteRepository';
+import { handleDefaultError } from '../utils/handleErrors';
 
 const NoteController = {
   store: {
     validations: NoteValidations.store,
-    handler: NoteRepository.store,
+    handler: async (req, res) => {
+      const { title, userId, content, color } = req.body;
+
+      try {
+        const note = await NoteRepository.store({
+          title,
+          userId,
+          content,
+          color,
+        });
+        return res.status(201).json({
+          data: note,
+          msg: 'Nota criada!',
+        });
+      } catch (error) {
+        return handleDefaultError(error, res);
+      }
+    },
   },
 };
 
--- src/repository/NoteRepository.js
+++ src/repository/NoteRepository.js
@@ -1,18 +1,12 @@
 import Note from '../models/Note';
-import { handleDefaultError } from '../utils/handleErrors';
 
 class NoteRepository {
-  async store(req, res) {
-    const { title, userId, content, color } = req.body;
-
+  async store(note) {
     try {
-      const note = await Note.create({ title, userId, content, color });
-      return res.status(201).json({
-        data: note,
-        msg: 'Nota criada!',
-      });
+      const newNote = await Note.create(note);
+      return newNote;
     } catch (error) {
-      return handleDefaultError(error, res);
+      return error;
     }
   }
 }
